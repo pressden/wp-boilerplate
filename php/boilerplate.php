31ce98b3baaf5608303558db2921f9723df62d21
D.S. Webster <dswebs.me@gmail.com>
2017-11-28 12:59:13 -0500

    Add customizer options to our boilerplate

--- php/boilerplate.php
+++ php/boilerplate.php
@@ -7,3 +7,4 @@
 require_once( 'initialize.php' );
 require_once( 'helpers.php' );
 require_once( 'actions.php' );
+require_once( 'customizer.php' );
---
+++ php/customizer.php
@@ -0,0 +1,30 @@
+<?php
+// CUSTOMIZER
+
+//* Add support for custom background
+add_theme_support( 'custom-background', array(
+	'default-color'         => 'ffffff',
+	'default-image'         => get_stylesheet_directory_uri() . '/images/bg.gif',
+	'wp-head-callback'      => 'childtheme_background_callback',
+) );
+
+//* Add custom background callback
+if( !function_exists( 'childtheme_background_callback' ) ) {
+	function childtheme_background_callback() { 
+
+		$background = get_background_image();
+		$color = get_background_color();
+
+		if ( ! $background && ! $color )  
+			return;
+
+		echo trim( sprintf( 
+			"<style type='text/css'>.custom-background .site-header-banner { background: %s %s %s %s %s; } </style>",
+			$background ? 'url('. $background .')' : '',
+			$color ? '#'. $color : 'transparent', 
+			get_theme_mod( 'background_repeat', 'repeat' ), 
+			get_theme_mod( 'background_position_x', 'left' ), 
+			get_theme_mod( 'background_attachment', 'scroll' )
+		) );
+	}
+}
