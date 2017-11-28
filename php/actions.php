da9027f54dfdea6ac5f47909910cd41cc2231311
D.S. Webster <dswebs.me@gmail.com>
2017-11-28 12:57:55 -0500

    Add the custom header banner

--- php/actions.php
+++ php/actions.php
@@ -1,4 +1,5 @@
 <?php
+// ACTIONS
 
 //* Enqueue styles
 add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_styles' );
@@ -18,5 +19,16 @@ if( !function_exists( 'childtheme_enqueue_scripts' ) ) {
 	function childtheme_enqueue_scripts() {
 		// main JS file (generated via webpack)
 		wp_enqueue_script( 'bundle', get_stylesheet_directory_uri() . '/dist/main.js', array( 'jquery' ), 1, false );
+	}
+}
+
+//* Add custom header banner after header
+add_action( 'genesis_after_header', 'childtheme_header_banner' );
+if( !function_exists( 'childtheme_header_banner' ) ) {
+	function childtheme_header_banner() {
+		if ( !get_background_image() )
+			return;
+
+		echo '<div class="site-header-banner"></div>';
 	}
 }
