b4d0c8b6c08126f4955bbc7098d6b1718880c865
Marty OConnor <marty@boultoncreative.com>
2018-01-22 13:58:14 -0500

    adds AOS.js and Parallax.js to webpack and imports them

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // JS
 require('./js/boilerplate');
 import 'bootstrap';
-
+import AOS from 'aos';
+import Parallax from 'parallax-js';
+//import './js/parallax-scene';
 // CSS && SCSS
 require('./scss/boilerplate.scss');
+
+// Call AOS initialize function from here, because of scope.
+jQuery( document ).ready(function() {
+	
+	AOS.init();
+	
+	//initialize parallax scene
+	var scene = document.getElementById('scene');
+	var parallaxInstance = new Parallax(scene);
+
+
+});
