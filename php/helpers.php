6e4be90ee1edeecbf796e1e9a90ccc60f6034188
D.S. Webster <dswebs.me@gmail.com>
2018-02-27 09:35:05 -0500

    Cleanup and debug helpers

--- php/helpers.php
+++ php/helpers.php
@@ -3,24 +3,18 @@
 
 // register taxonomy with preferred defaults
 if( !function_exists( 'childtheme_register_taxonomy' ) ) {
-	function childtheme_register_taxonomy($codename, $singular, $plural, $url_slug = null, $args = array())
+	function childtheme_register_taxonomy( $codename, $singular, $plural, $url_slug = null, $args = array() )
 	{
 		// define a useful url_slug since rewriting is encouraged
-		$url_slug = ($url_slug) ? $url_slug : $codename;
+		$url_slug = ( $url_slug ) ? $url_slug : $codename;
 
 		$defaults = array(
-			'applies_to' => array('post'),
-			'capabilities' => array(
-				'manage_terms'=> 'manage_' . $codename,
-				'edit_terms'=> 'edit_' . $codename,
-				'delete_terms'=> 'delete_' . $codename,
-				'assign_terms' => 'assign_' . $codename,
-			),
+			'applies_to' => array( 'post' ),
 			'hierarchical' => false,
-			'rewrite' => array('slug' => $url_slug),
+			'rewrite' => array( 'slug' => $url_slug ),
 		);
 
-		$args = wp_parse_args($args, $defaults);
+		$args = wp_parse_args( $args, $defaults );
 
 		$labels = array(
 			'name' => $plural,
@@ -32,9 +26,9 @@ if( !function_exists( 'childtheme_register_taxonomy' ) ) {
 			'update_item' => 'Update ' . $singular,
 			'add_new_item' => 'Add New ' . $singular,
 			'new_item_name' => 'New ' . $singular . ' Name',
-			'separate_items_with_commas' => 'Separate ' . strtolower($plural) . ' with commas',
-			'add_or_remove_items' => 'Add or remove ' . strtolower($plural),
-			'choose_from_most_used' => 'Choose from the most used ' . strtolower($plural),
+			'separate_items_with_commas' => 'Separate ' . strtolower( $plural ) . ' with commas',
+			'add_or_remove_items' => 'Add or remove ' . strtolower( $plural ),
+			'choose_from_most_used' => 'Choose from the most used ' . strtolower( $plural ),
 			'menu_name' => $plural,
 		);
 
@@ -50,10 +44,10 @@ if( !function_exists( 'childtheme_register_taxonomy' ) ) {
 
 // reguster post type with preferred defaults
 if( !function_exists( 'childtheme_register_post_type' ) ) {
-	function childtheme_register_post_type($codename, $singular, $plural, $url_slug = null, $args = array())
+	function childtheme_register_post_type( $codename, $singular, $plural, $url_slug = null, $args = array() )
 	{
 		// define a useful url_slug since rewriting is encouraged
-		$url_slug = ($url_slug) ? $url_slug : $codename;
+		$url_slug = ( $url_slug ) ? $url_slug : $codename;
 
 		$labels = array(
 			'name' => $plural,
@@ -63,37 +57,29 @@ if( !function_exists( 'childtheme_register_post_type' ) ) {
 			'new_item' => 'Create ' . $singular,
 			'view_item' => 'View ' . $singular,
 			'search_items' => 'Search ' . $singular,
-			'not_found' =>  'No ' . strtolower($plural) . ' found',
-			'not_found_in_trash' => 'No ' . strtolower($plural) . ' found in trash',
+			'not_found' =>  'No ' . strtolower( $plural ) . ' found',
+			'not_found_in_trash' => 'No ' . strtolower( $plural ) . ' found in trash',
 		);
 
 		$defaults = array(
 			'labels' => $labels,
 			'capability_type' => 'post',
-			'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes', 'post-formats'),
-			'taxonomies' => array('category', 'post_tag'),
-			'hierarchical' => false,
+			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes', 'post-formats' ),
 			'public' => true,
-			'publicly_queryable' => true,
-			'exclude_from_search' => false,
-			'show_ui' => true,
-			'show_in_menu' => true,
-			'show_in_nav_menus' => true,
-			'menu_position' => null,
-			'menu_icon' => null,
-			'rewrite' => array('slug' => $url_slug),
+			'menu_position' => 20,
+			'rewrite' => array( 'slug' => $url_slug ),
 			'has_archive' => true,
 		);
 
-		$args = wp_parse_args($args, $defaults);
+		$args = wp_parse_args( $args, $defaults );
 
-		register_post_type($codename, $args);
+		register_post_type( $codename, $args );
 	}
 }
 
 // register sidebar with preferred defaults
 if( !function_exists( 'childtheme_register_sidebar' ) ) {
-	function childtheme_register_sidebar($codename, $name, $args = array())
+	function childtheme_register_sidebar( $codename, $name, $args = array() )
 	{
 		$defaults = array(
 			'description' => '',
@@ -104,13 +90,13 @@ if( !function_exists( 'childtheme_register_sidebar' ) ) {
 			'after_title'   => '</h2>',
 		);
 
-		$args = wp_parse_args($args, $defaults);
+		$args = wp_parse_args( $args, $defaults );
 
 		$args['name'] = $name;
 		$args['id'] = $codename;
 
-		if(function_exists('register_sidebar')) {
-			register_sidebar($args);
+		if( function_exists( 'register_sidebar' ) ) {
+			register_sidebar( $args );
 		}
 	}
 }
