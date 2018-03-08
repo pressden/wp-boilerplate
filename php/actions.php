01ce42398ad0c51150e123fdc3ada210242b14a4
D.S. Webster <dswebs.me@gmail.com>
2018-03-08 09:17:57 -0500

    Revert the change to load Material Icons localy in favor of the CDN version

--- php/actions.php
+++ php/actions.php
@@ -6,7 +6,7 @@ add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_fonts' );
 if( !function_exists( 'childtheme_enqueue_fonts' ) ) {
 	function childtheme_enqueue_fonts() {
 		// Google fonts
-		wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700|Roboto:300,400,700', array(), CHILD_THEME_VERSION );
+		wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700|Roboto:300,400,700|Material+Icons', array(), CHILD_THEME_VERSION );
 	}
 }
 
