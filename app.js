9e240270fca021426df8a67f60c8fd6b0e416969
D.S. Webster <dswebs.me@gmail.com>
2018-07-20 06:42:51 -0400

    Remove AOS and Parallax to improve performance

--- app.js
+++ app.js
@@ -2,20 +2,6 @@
 require('./js/boilerplate');
 
 import 'bootstrap';
-import AOS from 'aos';
-import Parallax from 'parallax-js';
 
 // CSS && SCSS
 require('./scss/boilerplate.scss');
-
-// call AOS initialize function from here, because of scope.
-jQuery( document ).ready(function() {
-
-	AOS.init();
-
-	// check if scene exists and initialize
-	if (document.getElementById('scene')) {
-		var scene = document.getElementById('scene');
-		var parallaxInstance = new Parallax(scene);
-	}
-});
