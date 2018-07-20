6f85b4562e742b23b4a42eb4aa445203a5491ab5
D.S. Webster <dswebs.me@gmail.com>
2018-07-20 16:07:23 -0400

    Allow parent items in the menu to be clickable

--- js/boilerplate.js
+++ js/boilerplate.js
@@ -1,1 +1,10 @@
 /* begin boilerplate.js */
+
+$( document ).ready( function () {
+	// if a parent has an href attribute allow it to be clickable
+	$( '.dropdown-toggle' ).click( function() {
+		if( $( this ).attr( 'href' ) && $( this ).next( '.dropdown-menu' ).is( ':visible' ) ) {
+			window.location = $( this ).attr( 'href' );
+		}
+	});
+} );
