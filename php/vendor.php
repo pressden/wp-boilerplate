eb8c837ee3d7954073849953566bacaf205af3b9
D.S. Webster <dswebs.me@gmail.com>
2018-07-30 13:54:32 -0400

    Cleanup the vendor file and add the events calendar include

--- php/vendor.php
+++ php/vendor.php
@@ -5,7 +5,9 @@
 add_action( 'after_setup_theme', 'childtheme_include_third_party_files' );
 if( !function_exists( 'childtheme_include_third_party_files' ) ) {
 	function childtheme_include_third_party_files() {
-		require_once( get_stylesheet_directory() . '/boilerplate/php/lib/wp-bootstrap-navwalker.php' );
-		require_once( get_stylesheet_directory() . '/boilerplate/php/lib/genesis-title-toggle.php' );
+		$vendors_path = get_stylesheet_directory() . '/boilerplate/php/lib/';
+		require_once( $vendors_path . 'genesis-title-toggle.php' );
+		require_once( $vendors_path . 'the-events-calendar.php' );
+		require_once( $vendors_path . 'wp-bootstrap-navwalker.php' );
 	}
 }
