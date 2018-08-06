f677a54837db4dd81417b38708ec8f82b169dbf1
D.S. Webster <dswebs.me@gmail.com>
2018-08-06 09:30:25 -0400

    Make dropdowns hoverable by default

--- js/boilerplate.js
+++ js/boilerplate.js
@@ -1,10 +1,22 @@
 /* begin boilerplate.js */
 
 $( document ).ready( function () {
-	// if a parent has an href attribute allow it to be clickable
+
+	// add a hover event to our dropdowns
+	$( '.dropdown' ).hover(
+		function() {
+			$( this ).find( '.dropdown-menu' ).addClass( 'show' );
+		},
+		function() {
+			$( this ).find( '.dropdown-menu' ).removeClass( 'show' );
+		}
+	);
+
+	// allow dropdown parents with href attributes to be clickable
 	$( '.dropdown-toggle' ).click( function() {
 		if( $( this ).attr( 'href' ) && $( this ).next( '.dropdown-menu' ).is( ':visible' ) ) {
 			window.location = $( this ).attr( 'href' );
 		}
-	});
+	} );
+
 } );
