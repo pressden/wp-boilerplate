b50915c16a9859162d55cfeda2ea5004b25780ca
D.S. Webster <dswebs.me@gmail.com>
2019-01-09 13:19:38 -0500

    Add query modifiers for the work-item post type and work-type taxonomy

--- php/actions.php
+++ php/actions.php
@@ -67,7 +67,33 @@ add_filter( 'pre_get_posts', 'childtheme_modify_faq_archive_query' );
 if( !function_exists( 'childtheme_modify_faq_archive_query' ) ) {
 	function childtheme_modify_faq_archive_query( $query ) {
 		if ( $query->is_main_query() && $query->is_post_type_archive( 'faq' ) ) {
-			// remove the `posts_per_page` limit so all FAQ's load on a single page
+			// remove the `posts_per_page` limit so all entries load on a single page
+			$query->query_vars['posts_per_page'] = -1;
+		}
+
+		return $query;
+	}
+}
+
+// modify the work-item archive main query
+add_filter( 'pre_get_posts', 'childtheme_modify_work_item_archive_query' );
+if( !function_exists( 'childtheme_modify_work_item_archive_query' ) ) {
+	function childtheme_modify_work_item_archive_query( $query ) {
+		if ( $query->is_main_query() && $query->is_post_type_archive( 'work-item' ) ) {
+			// remove the `posts_per_page` limit so all entries load on a single page
+			$query->query_vars['posts_per_page'] = -1;
+		}
+
+		return $query;
+	}
+}
+
+// modify the work-type taxonomy main query
+add_filter( 'pre_get_posts', 'childtheme_modify_work_type_taxonomy_query' );
+if( !function_exists( 'childtheme_modify_work_type_taxonomy_query' ) ) {
+	function childtheme_modify_work_type_taxonomy_query( $query ) {
+		if ( $query->is_main_query() && $query->is_tax( 'work-type' ) ) {
+			// remove the `posts_per_page` limit so all entries load on a single page
 			$query->query_vars['posts_per_page'] = -1;
 		}
 
