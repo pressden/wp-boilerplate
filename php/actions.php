abc9e14737b3eb0580002760d4a2c5d3f58c62b7
D.S. Webster <dswebs.me@gmail.com>
2020-01-13 13:36:13 -0500

    Separate enqueue actions into a standalone file

--- php/actions.php
+++ php/actions.php
@@ -1,33 +1,6 @@
 <?php
 // ACTIONS
 
-// enqueue fonts
-add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_fonts' );
-if( !function_exists( 'childtheme_enqueue_fonts' ) ) {
-	function childtheme_enqueue_fonts() {
-		// Google fonts
-		wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700|Roboto:300,400,700|Material+Icons', array(), wp_get_theme()->get( 'Version' ) );
-	}
-}
-
-// enqueue styles
-add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_styles' );
-if( !function_exists( 'childtheme_enqueue_styles' ) ) {
-	function childtheme_enqueue_styles() {
-		// main CSS file (generated via webpack from SASS files)
-		wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/dist/main.css', array(), wp_get_theme()->get( 'Version' ) );
-	}
-}
-
-// enqueue scripts
-add_action( 'wp_enqueue_scripts', 'childtheme_enqueue_scripts' );
-if( !function_exists( 'childtheme_enqueue_scripts' ) ) {
-	function childtheme_enqueue_scripts() {
-		// main JS file (generated via webpack)
-		wp_enqueue_script( 'bundle', get_stylesheet_directory_uri() . '/dist/main.js', array( 'jquery' ), wp_get_theme()->get( 'Version' ), true );
-	}
-}
-
 // add the utility bar
 add_action( 'genesis_before_header', 'childtheme_add_utility_bar' );
 if( !function_exists( 'childtheme_add_utility_bar' ) ) {
--- php/boilerplate.php
+++ php/boilerplate.php
@@ -8,6 +8,7 @@
 require_once( 'initialize.php' );
 require_once( 'bootstrap.php' );
 require_once( 'helpers.php' );
+require_once( 'enqueue.php' );
 require_once( 'actions.php' );
 require_once( 'filters.php' );
 require_once( 'customizer.php' );
