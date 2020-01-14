a19ecc4dfb736c0d41a7a71b4c66a8b385408d23
D.S. Webster <dswebs.me@gmail.com>
2020-01-14 09:35:06 -0500

    Rename the constant to be more descriptive

--- php/boilerplate.php
+++ php/boilerplate.php
@@ -5,15 +5,15 @@
  * This file includes all files required by wp-boilerplate.
  **********************************/
 
-define( 'BOILERPLATE_DIRECTORY', get_stylesheet_directory() . '/boilerplate' );
+define( 'BOILERPLATE_PHP_DIR', dirname( __FILE__ ) );
 
 // load core files
-require_once( BOILERPLATE_DIRECTORY . '/php/initialize.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/bootstrap.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/helpers.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/enqueue.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/actions.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/filters.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/customizer.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/vendor.php' );
-require_once( BOILERPLATE_DIRECTORY . '/php/widgets.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/initialize.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/bootstrap.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/helpers.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/enqueue.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/actions.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/filters.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/customizer.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/vendor.php' );
+require_once( BOILERPLATE_PHP_DIR . '/php/widgets.php' );
