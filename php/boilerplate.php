c8ce548d58820b00e1c6846aa9a57b5bd9e8bc14
D.S. Webster <dswebs.me@gmail.com>
2020-01-14 08:52:13 -0500

    Path the required files to prevent loading the wrong file

--- php/boilerplate.php
+++ php/boilerplate.php
@@ -4,13 +4,16 @@
  *
  * This file includes all files required by wp-boilerplate.
  **********************************/
+
+define( 'BOILERPLATE_DIRECTORY', get_stylesheet_directory() . '/boilerplate' );
+
 // load core files
-require_once( 'initialize.php' );
-require_once( 'bootstrap.php' );
-require_once( 'helpers.php' );
-require_once( 'enqueue.php' );
-require_once( 'actions.php' );
-require_once( 'filters.php' );
-require_once( 'customizer.php' );
-require_once( 'vendor.php' );
-require_once( 'widgets.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/initialize.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/bootstrap.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/helpers.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/enqueue.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/actions.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/filters.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/customizer.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/vendor.php' );
+require_once( BOILERPLATE_DIRECTORY . '/php/widgets.php' );
