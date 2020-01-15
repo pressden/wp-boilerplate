35faa0b06c4ba7765a1ea74e71e219ad0d7fefe6
D.S. Webster <dswebs.me@gmail.com>
2020-01-15 08:29:35 -0500

    Remove customizer code as it is completely reworked in Emma

--- php/boilerplate.php
+++ php/boilerplate.php
@@ -14,6 +14,5 @@ require_once( BOILERPLATE_PHP_DIR . '/helpers.php' );
 require_once( BOILERPLATE_PHP_DIR . '/enqueue.php' );
 require_once( BOILERPLATE_PHP_DIR . '/actions.php' );
 require_once( BOILERPLATE_PHP_DIR . '/filters.php' );
-require_once( BOILERPLATE_PHP_DIR . '/customizer.php' );
 require_once( BOILERPLATE_PHP_DIR . '/vendor.php' );
 require_once( BOILERPLATE_PHP_DIR . '/widgets.php' );
