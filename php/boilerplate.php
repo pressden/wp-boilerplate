6c9d2988a8828db3d88a01c2fc2265ca15e29f68
D.S. Webster <dswebs.me@gmail.com>
2020-01-29 08:40:10 -0500

    Remove Boilerplate enqueues in favor of Emma enqueues

--- php/boilerplate.php
+++ php/boilerplate.php
@@ -10,7 +10,6 @@ define( 'BOILERPLATE_PHP_DIR', dirname( __FILE__ ) );
 // load core files
 require_once( BOILERPLATE_PHP_DIR . '/initialize.php' );
 require_once( BOILERPLATE_PHP_DIR . '/helpers.php' );
-require_once( BOILERPLATE_PHP_DIR . '/enqueue.php' );
 require_once( BOILERPLATE_PHP_DIR . '/actions.php' );
 require_once( BOILERPLATE_PHP_DIR . '/filters.php' );
 require_once( BOILERPLATE_PHP_DIR . '/vendor.php' );
